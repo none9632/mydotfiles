145755eae7198079b1231821581b20b0b1da9866
none9632 <ivanandreev1234@protonmail.com>
2022-01-09 16:17:56 +0000

    Improved updates script in polybar

--- .config/polybar/scripts/updates.sh
+++ .config/polybar/scripts/updates.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-NOTIFY_ICON=/usr/share/icons/Papirus/32x32/apps/system-software-update.svg
+notify_icon=/usr/share/icons/Papirus/32x32/apps/system-software-update.svg
+cache_dir=$HOME/.cache/updates
+yay_log_file=$cache_dir/updates.log
 
-get_total_updates() {
-    sudo yay -Syy 2>~/.cache/updates/updates.log
+get_total_updates ()
+{
+    # sudo yay -Syy 2>~/.cache/updates/updates.log
+    update
     updates=$(yay -Qu 2>/dev/null | wc -l);
 }
 
-while true; do
+while true
+do
     echo "  0"
     sleep 1
     get_total_updates
@@ -15,13 +20,20 @@ while true; do
     # notify user of updates
     if hash notify-send &>/dev/null && [ $updates -gt 0 ]
     then
-        notify-send -u normal -i $NOTIFY_ICON \
+        notify-send -u normal -i $notify_icon \
                     "You should update soon" "$updates New packages"
     fi
+    if [ $(cat $log_file | wc -l) -gt 0 ]
+    then
+        notify-send -u normal -i $notify_icon \
+                    "Error in updates script" "Something wrong"
+    fi
+
 
     # when there are updates available
     # every 100 seconds another check for updates is done
-    while (( $updates > 0 )); do
+    while (( $updates > 0 ))
+    do
         if (( $updates > 99 )); then
             echo "$updates"
         elif (( $updates > 9 )); then
@@ -35,7 +47,8 @@ while true; do
 
     # when no updates are available, use a longer loop, this saves on CPU
     # and network uptime, only checking once every 10 min for new updates
-    while (( $updates == 0 )); do
+    while (( $updates == 0 ))
+    do
         echo "  0"
         sleep 600
         get_total_updates
