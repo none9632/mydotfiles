8bdf191b3c06da9a215688126527d2dc8eeeefd8
none9632 <ivanandreev1234@protonmail.com>
2022-03-07 20:19:00 +0000

    Improved install_bin function in install script

--- install.sh
+++ install.sh
@@ -41,29 +41,28 @@ function install_bin ()
 {
     for file in $bin_files
     do
-        if [ ! -f $bin_dir/$file ]
+        if [ -e $bin_dir/$file ]
         then
-            echo "Creating symlink to $file in $bin_dir"
-            ln -s $dot_dir/bin/$file $bin_dir/$file
+            echo "Moving $file from ~/.local/bin/ to $old_dots_dir/.local/bin"
+            mv ~/.local/bin/$file $old_dots_dir/.local/bin
         fi
+    done
+    for file in $bin_files
+    do
+        echo "Creating symlink to $file in $bin_dir"
+        ln -s $dot_dir/bin/$file $bin_dir/$file
     done
 }
 
 function install_config ()
 {
-    # create dotfiles_old in homedir
-    [ -d $old_dots_dir ] && rm -rf $old_dots_dir
-    echo "Creating $old_dots_dir for backup of any existing config files"
-    mkdir -p $old_dots_dir
-    mkdir -p $old_dots_dir/.config
-
     # move any existing dotfiles in homedir to directory, then create symlinks
     for file in $config_files
     do
         if [ -d ~/.config/$file ]
         then
             echo "Moving $file from ~/.config to $old_dots_dir/.config"
-            mv ~/.config/$file $old_dots_dir/.config 2> /dev/null
+            mv ~/.config/$file $old_dots_dir/.config
         fi
     done
     for file in $config_files
@@ -98,6 +97,13 @@ function install_pkgs ()
     # install the necessary packages
     yay --noconfirm --needed -S $pkgs
 }
+
+# create dotfiles_old in homedir
+[ -d $old_dots_dir ] && rm -rf $old_dots_dir
+echo "Creating $old_dots_dir for backup of any existing config files"
+mkdir -p $old_dots_dir
+mkdir -p $old_dots_dir/.config
+mkdir -p $old_dots_dir/.local/bin
 
 case "$1" in
     ""|all)   install_all    ;;
