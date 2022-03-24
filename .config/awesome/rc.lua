25a6a28d44eed362a58effea35af087f757d6fbe
none9632 <ivanandreev1234@protonmail.com>
2022-03-24 21:19:28 +0000

    Changed rules in awesome

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -323,33 +323,45 @@ ruled.client.connect_signal("request::rules", function()
     }
 
     -- Floating clients.
+    -- ruled.client.append_rule {
+    --    id       = "floating",
+    --    rule_any = {
+    -- 	  instance = { "copyq", "pinentry" },
+    -- 	  class    = {
+    -- 		 "Arandr", "Blueman-manager", "Kruler", "Sxiv",
+    -- 		 "Wpa_gui", "veromix", "xtightvncviewer"
+    -- 	  },
+    -- 	  -- Note that the name property shown in xprop might be set slightly after creation of the client
+    -- 	  -- and the name shown there might not match defined rules here.
+    -- 	  name    = {
+    -- 		 "Event Tester",  -- xev.
+    -- 	  },
+    -- 	  role    = {
+    -- 		 "AlarmWindow",    -- Thunderbird's calendar.
+    -- 		 "ConfigManager",  -- Thunderbird's about:config.
+    -- 		 "pop-up",         -- e.g. Google Chrome's (detached) Developer Tools.
+    -- 	  }
+    --    },
+    --    properties = { floating = true }
+    -- }
+
     ruled.client.append_rule {
-        id       = "floating",
-        rule_any = {
-            instance = { "copyq", "pinentry" },
-            class    = {
-                "Arandr", "Blueman-manager", "Gpick", "Kruler", "Sxiv",
-                "Tor Browser", "Wpa_gui", "veromix", "xtightvncviewer"
-            },
-            -- Note that the name property shown in xprop might be set slightly after creation of the client
-            -- and the name shown there might not match defined rules here.
-            name    = {
-                "Event Tester",  -- xev.
-            },
-            role    = {
-                "AlarmWindow",    -- Thunderbird's calendar.
-                "ConfigManager",  -- Thunderbird's about:config.
-                "pop-up",         -- e.g. Google Chrome's (detached) Developer Tools.
-            }
-        },
-        properties = { floating = true }
+       rule       = { class = "firefox"     },
+       properties = { screen = 1, tag = " 7 " }
     }
 
-    -- Set Firefox to always map on the tag named "2" on screen 1.
-    -- ruled.client.append_rule {
-    --     rule       = { class = "Firefox"     },
-    --     properties = { screen = 1, tag = "2" }
-    -- }
+    ruled.client.append_rule {
+       rule_any   = {
+          class = {
+             "Rofi",
+             "Gpick"
+          }
+       },
+       properties = {
+          floating = true,
+          ontop    = true,
+       }
+    }
 end)
 
 local screen = awful.screen.focused()
