b10a4d57cde926a07840301b224026ad190acd95
none9632 <none9632@protonmail.com>
2022-08-28 14:50:09 +0300

    bin: improved ram

--- bin/bar/ram.c
+++ bin/bar/ram.c
@@ -1,38 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <regex.h>
+#include <string.h>
 
-#define SIZE 28
+#define SIZE 64
 
-#define mem_available_s buffers_s
-#define swap_cached_s   shmem_s
-#define active_s        shmem_s
-#define inactive_s      shmem_s
-#define active_anon_s   shmem_s
-#define inactive_anoa_s shmem_s
-#define active_file_s   shmem_s
-#define inactive_file_s shmem_s
-#define unevictable_s   shmem_s
-#define mlocked_s       shmem_s
-#define swap_total_s    shmem_s
-#define swap_free_s     shmem_s
-#define dirty_s         shmem_s
-#define writeback_s     shmem_s
-#define anon_pages_s    shmem_s
-#define mapped_s        shmem_s
-#define kreclaimable_s  sreclaimable_s
-#define slab_s          sreclaimable_s
+char* regex_search(FILE *file, char *regexp, char *line)
+{
+  regex_t re;
+
+  if (regcomp(&re, regexp, REG_EXTENDED) != 0)
+    exit(EXIT_FAILURE);
+
+  while ((fgets(line, 64, file)) != NULL)
+    {
+      line[strlen(line)-1] = '\0';
+      if (regexec(&re, line, 0, NULL, 0) == 0)
+        return line;
+    }
+}
 
 int take_num(char *str)
 {
   int num = 0;
 
-  for (int i = 16; i < SIZE - 4; ++i)
+  for (int i = 16; i < strlen(str) - 3; ++i)
     {
       char ch = str[i];
-
       if (ch == ' ')
         continue;
-
       num = num * 10 + (ch - '0');
     }
 
@@ -52,37 +48,19 @@ int main(void)
   if (meminfo == NULL)
     exit(EXIT_FAILURE);
 
-  fread(mem_total_s,     sizeof(char), SIZE, meminfo);
-  fread(mem_free_s,      sizeof(char), SIZE, meminfo);
-  fread(buffers_s,       sizeof(char), SIZE, meminfo);
-  fread(mem_available_s, sizeof(char), SIZE, meminfo);
-  fread(cached_s,        sizeof(char), SIZE, meminfo);
-  fread(swap_cached_s,   sizeof(char), SIZE, meminfo);
-  fread(active_s,        sizeof(char), SIZE, meminfo);
-  fread(inactive_s,      sizeof(char), SIZE, meminfo);
-  fread(active_anon_s,   sizeof(char), SIZE, meminfo);
-  fread(inactive_anoa_s, sizeof(char), SIZE, meminfo);
-  fread(active_file_s,   sizeof(char), SIZE, meminfo);
-  fread(inactive_file_s, sizeof(char), SIZE, meminfo);
-  fread(unevictable_s,   sizeof(char), SIZE, meminfo);
-  fread(mlocked_s,       sizeof(char), SIZE, meminfo);
-  fread(swap_total_s,    sizeof(char), SIZE, meminfo);
-  fread(swap_free_s,     sizeof(char), SIZE, meminfo);
-  fread(dirty_s,         sizeof(char), SIZE, meminfo);
-  fread(writeback_s,     sizeof(char), SIZE, meminfo);
-  fread(anon_pages_s,    sizeof(char), SIZE, meminfo);
-  fread(mapped_s,        sizeof(char), SIZE, meminfo);
-  fread(shmem_s,         sizeof(char), SIZE, meminfo);
-  fread(kreclaimable_s,  sizeof(char), SIZE, meminfo);
-  fread(slab_s,          sizeof(char), SIZE, meminfo);
-  fread(sreclaimable_s,  sizeof(char), SIZE, meminfo);
+  regex_search(meminfo, "MemTotal:",     mem_total_s);
+  regex_search(meminfo, "MemFree:",      mem_free_s);
+  regex_search(meminfo, "Buffers:",      buffers_s);
+  regex_search(meminfo, "Cached:",       cached_s);
+  regex_search(meminfo, "Shmem:",        shmem_s);
+  regex_search(meminfo, "SReclaimable:", sreclaimable_s);
 
   int mem_total    = take_num(mem_total_s),
-    mem_free     = take_num(mem_free_s),
-    buffers      = take_num(buffers_s),
-    shmem        = take_num(shmem_s),
-    sreclaimable = take_num(sreclaimable_s),
-    cached       = take_num(cached_s) + sreclaimable - shmem;
+      mem_free     = take_num(mem_free_s),
+      buffers      = take_num(buffers_s),
+      shmem        = take_num(shmem_s),
+      sreclaimable = take_num(sreclaimable_s),
+      cached       = take_num(cached_s) + sreclaimable - shmem;
 
   int used = mem_total - (mem_free + buffers + cached);
   double total_M = mem_total / 1024;
